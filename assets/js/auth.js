f3b24a363f909c743640df463c2e2530b339c601
Victor-Pointon <45980673+Victor-Pointon@users.noreply.github.com>
2020-03-17 16:37:48 +0200

    Update auth.js

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -1,11 +1,26 @@
 // signup
 const signupForm = document.querySelector('#signup-form');
 signupForm.addEventListener('submit', (e) => {
-    ();
+    e.preventDefault();
 
     // get user info
     const email = signupForm['sign-up-email'].value;
     const password = signupForm['sign-up-password'].value;
 
-    console.log(email, password)
-})
+    // sign up the user
+    auth.createUserWithEmailAndPassword(email, password).then(cred => {
+        // close the signup modal & reset form
+        const modal = document.querySelector('#modal-signup');
+        M.Modal.getInstance(modal).close();
+        signupForm.reset();
+    });
+});
+
+// logout
+const logout = document.querySelector('#logout');
+logout.addEventListener('click', (e) => {
+    e.preventDefault();
+    auth.signOut().then(() => {
+        console.log('user signed out');
+    })
+});
